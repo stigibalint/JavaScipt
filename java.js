dea34c292a52817086de12b46123703c3d91953d
stigibalint <72031994+stigibalint@users.noreply.github.com>
2022-10-28 14:01:22 +0200

    :D

---
+++ java.js
@@ -0,0 +1,8 @@
+
+function myFunction() {
+    document.getElementById("visszaigazol_nev").innerHTML = document.getElementById("mNeve").value;
+    document.getElementById("visszaigazol_cim").innerHTML = document.getElementById("mCim").value;
+    document.getElementById("visszaigazol_tel").innerHTML = document.getElementById("mTel").value;
+
+ 
+}
